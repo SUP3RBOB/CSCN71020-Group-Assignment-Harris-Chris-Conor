751fd5c92badee0d28a853650e32bc90363a2143
Connor Miller <miller77fb@gmail.com>
2023-11-10 08:57:28 -0500

    Finished adding rectangle input

--- PolygonChecker/main.h
+++ PolygonChecker/main.h
@@ -1,7 +1,5 @@
 #pragma once
-#include "vector.h"
+
 void printWelcome();
 int printShapeMenu();
 int* getTriangleSides(int* triangleSides);
-
-Vector2 setVector(float* x, float* y);
--- PolygonChecker/vector.h
+++ PolygonChecker/vector.h
@@ -8,5 +8,7 @@ typedef struct {
 
 float Distance(Vector2 start, Vector2 end);
 bool IsRectangle(Vector2 v1, Vector2 v2, Vector2 v3, Vector2 v4);
-float GetRectangleArea(Vector2 v1, Vector2 v2, Vector2 v3, Vector2 v4);
+float GetRectangleArea(float side1, float side2);
 float GetPerimeter(Vector2 v1, Vector2 v2, Vector2 v3, Vector2 v4);
+
+Vector2 setVector(float x, float y);
