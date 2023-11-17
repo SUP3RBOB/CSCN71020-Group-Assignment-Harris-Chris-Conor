05d0975ec6548b56b5ffbbefc30ce3c1cca8640c
PlatonicGecko <145587987+PlatonicGecko@users.noreply.github.com>
2023-11-17 08:11:25 -0500

    Added seperate unit test for each sln

---
+++ Vector_unit_tests/Vector_unit_tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Vectorunittests
+{
+	TEST_CLASS(Vectorunittests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
